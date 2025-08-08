a150c9955a0281ab9c2291e462ef9d1ea6ba54d2
Nazli2 <deniznazlisahin@gmail.com>
2025-08-08 23:48:56 +0300

    Ders 22 sonu

---
+++ Frontends/MovieApi.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers;
+public class AdminLayoutController : Controller
+{
+    public IActionResult LayoutUI()
+    {
+        return View();
+    }
+}
